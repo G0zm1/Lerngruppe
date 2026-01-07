96d30ccce0d9b8cd5b051cb6f946fe418ce23cdf
Dennis Voigt <dennisvoigt1995@gmail.com>
2026-01-07 17:22:39 +0100

    Add script to delete first n files from a directory
    Implement a shell script to delete the first n files in a specified directory, with error handling for parameters and directory existence.

---
+++ Kapitel_3/Aufgabe3.sh
@@ -0,0 +1,27 @@
+#Schreiben Sie ein Shell-Script, das die ersten n Files eines Ordners löscht. 
+#Das Verzeichnis und die Anzahl der zu löschenden Dateien sollen als Parameter beim Aufrufen angegeben werden.
+
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "please enter path and number of files as parameter."
+  echo "for example: ~/path/to/file 2"
+  exit 2
+fi
+
+path="$1"
+number="$2"
+
+if [ -d "$path" ]; then
+  echo "in "$1" were "$2" delete."
+
+  ls -1 "$path" | head -n "$number" | while read file; do
+    rm "$path/$file"
+
+  echo " "$file" was deleted."
+
+else
+  echo " "$path" dont exist."
+  exit 2
+
+fi
